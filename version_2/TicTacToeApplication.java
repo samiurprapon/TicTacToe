c93f2c74f64d1dddf0572c6d8637de6238f0fa48
Tanveer Ahmed <tanveerahmed7890@gmail.com>
2020-11-09 13:09:57 +0600

    movied previous files to version_2

---
+++ version_2/TicTacToeApplication.java
@@ -0,0 +1,15 @@
+/**
+ * Contains the main method. Creates a ConsoleRunner and then calls its
+ * mainLoop method.
+ *
+ * @author Tanveer Ahmed
+ */
+
+public class TicTacToeApplication {
+
+    public static void main(String[] args) {
+        /*
+         * TBD
+         */
+    }
+}
